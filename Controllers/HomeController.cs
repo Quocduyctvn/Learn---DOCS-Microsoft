3757f2d155f45055da44c28481a2a734731c8fcd
Quocduyctvn <quocduyctvn@gmail.com>
2025-02-11 21:17:22 +0700

    Create handleHomecontroller

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿
+// controller được gọi mặc định khi tao trang web lần đầu 
+
+using Microsoft.AspNetCore.Mvc;
+namespace MvcMovie.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+        public IActionResult UnauthorizedResult()
+        {
+            return Unauthorized();
+        }
+    }
+}
